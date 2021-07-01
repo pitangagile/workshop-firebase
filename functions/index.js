36fa2cf4e7c794b4fddc733f8854ee0108511fcf
Rodrigo Nunes <rodrigonunesc@Rodrigos-MacBook-Pro.local>
2021-07-01 15:48:48 -0300

    cloud function implementada

--- functions/index.js
+++ functions/index.js
@@ -4,21 +4,30 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore();
 
-//exemplos firestore: https://firebase.google.com/docs/firestore/quickstart?hl=pt-br
-
 //notifica usuário quando um novo despacho é criado pela primeira vez. Esta função é ativada quando um novo despacho é inserido no Firestore.
-//exemplo gatilhos cloud functions https://firebase.google.com/docs/functions/firestore-events
-exports.notify = //sua funcao aqui
+exports.notify = functions.firestore
+  .document('despachos/{docId}')
+  .onCreate(async (snap, context) => {
+    //coleta o json do novo despacho inserido no firestore
+    const newDispatch = snap.data();
+
+    const { responsavel } = newDispatch;
+
+    await sendNotification(responsavel);
+
+    return 'Notificação enviada com sucesso';
+  });
 
 
 async function sendNotification(responsibleUser) {
   //No firestore: Coleta o usuário referenciado no campo responsibleUser
-
+  const doc = await db.collection('users').doc(responsibleUser).get();
+  const user = doc.data();
   const fetchConfigs = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': '' //seu token aqui
+      'Authorization': 'sua Key aqui'
     },
     body: JSON.stringify({
       notification: {
@@ -26,7 +35,7 @@ async function sendNotification(responsibleUser) {
         "body": "Você tem um novo despacho disponível para visualização",
         "click_action": "http://localhost:5000"
       },
-      to: '' //token do usuario
+      to: user.token
     })
   };
   return fetch('https://fcm.googleapis.com/fcm/send', fetchConfigs);
